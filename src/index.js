5481947e260ac2fdab38f4210d3bdcad6dad1c00
Aaron Smith <smitham50@gmail.com>
2019-06-26 20:11:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const formTag = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+
+  formTag.addEventListener('submit', function(e) {
+
+    e.preventDefault()
+
+    const userInput = document.querySelector("#new-task-description").value
+    const taskItem = document.createElement("li")
+
+    const listItem = taskList.appendChild(taskItem)
+
+
+    listItem.innerText = userInput
+
+
+    const deleteButton = document.createElement("button")
+
+    deleteButton.innerText = "X"
+
+    taskItem.appendChild(deleteButton)
+
+    taskItem.addEventListener('click', function(e) {
+      e.target.parentElement.remove()
+    })
+
+    const dropdown = document.querySelector("#dropdown")
+
+    if (dropdown.value === "Red") {
+      listItem.style.color = "red"
+    }
+    if (dropdown.value === "Yellow") {
+      listItem.style.color = "yellow"
+    }
+    if (dropdown.value === "Green") {
+      listItem.style.color = "green"
+    }
+
+    formTag.reset()
+
+  })
 });
